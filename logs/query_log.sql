501995e56ee7385d1670faf4f966f879bf54ca22
rdiesveld-ops <rdiesveld@ilionx.com>
2026-09-18 16:19:45 +0200

    -

--- logs/query_log.sql
+++
@@ -1,9 +0,0 @@
--- created_at: 2026-09-18T13:30:09.963567700+00:00
--- finished_at: 2026-09-18T13:30:10.201673300+00:00
--- elapsed: 238ms
--- outcome: success
--- dialect: snowflake
--- node_id: not available
--- query_id: 01c7276a-030b-0ed8-0006-1a6a000612f2
--- desc: not provided
-select 1 as id;
